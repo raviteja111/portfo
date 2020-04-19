a11b6ca7e5273d7a7fd88924490cc207b379013d
rtdasari <raviinfo44@gmail.com>
2020-04-19 15:43:49 +0530

    updates

---
+++ server.py
@@ -0,0 +1,38 @@
+import csv
+from flask import Flask,render_template,url_for,request,redirect
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def htmlpage(page_name):
+	return render_template(page_name)
+
+def wrte_to_file(data):
+	with open('database.txt',mode='a') as database:
+		email =data["email"]
+		subject =data["subject"]
+		message =data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
+def wrte_to_csv(data):
+	with open('database2.csv',mode='a') as database2:
+		email =data["email"]
+		subject =data["subject"]
+		message =data["message"]
+		print(csv)
+		csv_writer = csv.writer(database2,delimiter=',',  quotechar=':',quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+	if request.method=='POST':
+		data = request.form.to_dict()
+		print(data)
+		wrte_to_csv(data)
+		return redirect('/thankyou.html')
+	else:
+		return 'something wrong'
